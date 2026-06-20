0816fee13e4496316403e9e52a2f64181d4f2bf0
TomoyukiMizuma <nishino123tomo@gmail.com>
2026-06-20 14:13:07 +0900

    deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+declare const process: {
+  env?: Record<string, string | undefined>;
+};
+
+const isGithubPages = process.env.DEPLOY_TARGET === "github-pages";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: isGithubPages ? "/svelte/" : "/",
   plugins: [svelte()],
 })
