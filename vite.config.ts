c9cbe69b98116c1aea6a6f4aa938aa827571a4d4
TomoyukiMizuma <nishino123tomo@gmail.com>
2026-06-30 10:29:25 +0900

    deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 // https://vite.dev/config/
+// https://vite.dev/config/shared-options#base
+// https://vite.dev/guide/build#public-base-path
 // https://github.com/kododake/PrintTools/blob/main/vite.config.ts
+// https://github.com/shuji-bonji/svelte5-blog-example/blob/main/svelte.config.js
 
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+  base: process.env.DEPLOY_TARGET === 'github-pages' ? "/svelte/" : "/",
+  // base: process.argv.includes('dev') ? '/' : process.env.BASE_PATH,
+
   plugins: [svelte()],
 })
